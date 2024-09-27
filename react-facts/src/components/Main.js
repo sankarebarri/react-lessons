220fe03487bb11e385ff024a4a39d17e9b54f322
sankarebarri <sankarebarri_dev@yahoo.com>
2024-09-27 09:54:39 +0000

    react.js

--- react-facts/src/components/Main.js
+++ react-facts/src/components/Main.js
@@ -1,7 +1,14 @@
 const Main = () => {
   return (
     <main>
-      <h1>Main Components</h1>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
+        <li>Was first realease in 2013</li>
+        <li>Was originally created by Jordan Walker</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers 1000s of entreprise apps, inculding mobile apps</li>
+      </ul>
     </main>
   );
 };
