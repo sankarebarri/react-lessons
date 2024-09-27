69943e1ae9f922f001af7b93bea7801f7b74f4a9
sankarebarri <sankarebarri_dev@yahoo.com>
2024-09-27 09:22:35 +0000

    react.js

---
+++ react-facts/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <div>
+      <Header />,
+      <Main />
+    </div>
+  );
+};
+
+export default App;
---
+++ react-facts/src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="../logo512.png" className="nav--icon" />
+        <h3 className="nav--logo-text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
