5df1ce47efd96477197c145c2e2ba833c0edc850
sankarebarri <sankarebarri_dev@yahoo.com>
2024-09-28 14:05:35 +0000

    airbnb

--- air-bnb/src/App.js
+++ air-bnb/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   );
 };
