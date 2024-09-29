cbcbe64f707a5f988f973068b60598aa9b055c79
sankarebarri <sankarebarri_dev@yahoo.com>
2024-09-29 16:44:33 +0000

    react.js

--- react-facts/src/App.js
+++ react-facts/src/App.js
@@ -1,11 +1,35 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <div>
-      <Header />,
-      <Main />
+      <Navbar />
+      <Hero />
+      <Card
+        img="katie-zaferes.png"
+        star="star.jpg"
+        rating={3}
+        country="USA"
+        price={136}
+        title="Life lessons with Katie Zaferes"
+      />
+      {/* <Contact
+        img="images/mountain-bike.png"
+        star="images/star.jpg"
+        rating="3"
+      />
+      <Contact
+        img="images/wedding-photography.png"
+        star="images/star.jpg"
+        rating="4"
+      />
+      <Contact
+        img="images/katie-zaferes.png"
+        star="images/star.jpg"
+        rating="5"
+      /> */}
     </div>
   );
 };
---
+++ react-facts/src/components/Card.js
@@ -0,0 +1,23 @@
+const Card = (props) => {
+  return (
+    <div className="card">
+      <img src={`images/${props.img}`} className="card--image" />
+      <div className="card--stats">
+        <img
+          src={`images/${props.star}`}
+          alt="star-image"
+          className="card--star"
+        />
+        <span>5.0</span>
+        <span className="gray">({props.rating}) .</span>
+        <span className="gray">{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        From <span className="bold">${props.price}</span> / person
+      </p>
+    </div>
+  );
+};
+
+export default Card;
