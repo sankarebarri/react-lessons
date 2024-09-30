63ced43804417a4d1790d3623728633937049c41
sankarebarri <sankarebarri_dev@yahoo.com>
2024-09-30 10:10:39 +0000

    react.js

--- air-bnb/src/App.js
+++ air-bnb/src/App.js
@@ -1,12 +1,35 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import Contact from "./components/Contact";
+import data from "./data";
+
 const App = () => {
+  // console.log(data[0].openSpots);
+
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        // img={item.coverImg}
+        // star={item.stats.rating}
+        // rating={item.stats.reviewCount}
+        // country={item.location}
+        // price={item.price}
+        // title={item.title}
+        // openSpots={item.openSpots}
+        // location={item.location}
+
+        // item={item}
+        {...item}
+      />
+    );
+  });
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <section className="cards--list">{cards}</section>
     </div>
   );
 };
--- air-bnb/src/components/Card.js
+++ air-bnb/src/components/Card.js
@@ -1,16 +1,25 @@
-const Card = () => {
+const Card = (props) => {
+  // console.log(props.img);
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
-      <img src="images/katie-zafere.png" alt="katie-zafere" />
+      {/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`images/${props.coverImg}`} className="card--image" />
       <div className="card--stats">
-        <img src="images/star.jpg" alt="star-image" className="card--start" />
+        <img src="images/star.jpg" alt="star-image" className="card--star" />
         <span>5.0</span>
-        <span className="gray">(6) .</span>
-        <span className="gray">USA</span>
+        <span className="gray">({props.rating}) .</span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
+      <p>{props.title}</p>
       <p>
-        From <span className="bold">$136</span> / person
+        From <span className="bold">${props.price}</span> / person
       </p>
     </div>
   );
