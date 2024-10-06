7dc56a001247bed481d73ce31e8574ad21c54257
sankarebarri <sankarebarri_dev@yahoo.com>
2024-10-06 13:51:02 +0000

    memeGenerator

---
+++ memeGenerator/src/components/Forms.js
@@ -0,0 +1,126 @@
+import React from "react";
+const Forms = () => {
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isFriendly: true,
+    employment: "",
+    favColor: "",
+  });
+  // console.log(formData.favColor);
+
+  const handleChange = (event) => {
+    // console.log(event.target.checked);
+
+    const { name, value, type, checked } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        // [event.target.name]: event.target.value,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.prevetDefault();
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="First Name"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        onChange={handleChange}
+        value={formData.email}
+      />
+      <textarea
+        type="text"
+        placeholder="Your text here..."
+        name="comments"
+        onChange={handleChange}
+        value={formData.textarea}
+      />
+      <input
+        type="checkbox"
+        id="isFriendly"
+        checked={formData.isFriendly}
+        onChange={handleChange}
+        name="isFriendly"
+      />
+      <label htmlFor="isFriendly">Are you Friendly?</label>
+
+      <fieldset>
+        <legend>Current Employment Status</legend>
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full time</label>
+        <br />
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part time</label>
+        <br />
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+      </fieldset>
+      <br />
+      <label htmlFor="favColor">What is your favorite color?</label>
+      <br />
+      <select
+        id="favColor"
+        value={formData.favColor}
+        name="favColor"
+        onChange={handleChange}
+      >
+        <option value="">--Choose--</option>
+        <option value="red">Red</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+      </select>
+      <br />
+      {/* <input type="submit" value="Submit" /> */}
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Forms;
